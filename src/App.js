c4eb96cd9de7d8d43285dba55191b7ee7016185a
leonardoGomes <leonardojpinheirogomes@gmail.com>
2023-11-22 22:15:50 -0300

    feat: Start Annoucement component

--- src/App.js
+++ src/App.js
@@ -25,7 +25,11 @@ function App() {
         step3="Step 3 : Special Offer"
         step4="Step 4 : Confirmation"
         />
-        <Annoucement />
+        <Annoucement 
+        profileName="Ken T." 
+        verifiedStatusProfile="Verified Customer"
+        comentText="“As soon as the Clarifions arrived I put one in my bedroom. This was late in the afternoon. When I went to the bedroom in the evening it smelled clean. When I went to bed I felt I could breathe better. Wonderful.”"
+        />
     </>
   );
 }
--- src/Components/Announcement/index.jsx
+++ src/Components/Announcement/index.jsx
@@ -1,12 +1,42 @@
-import "./styles.css"
+import "./styles.css";
+import imgAnnoucement from "./assets/imgAnnoucement.svg";
+import stars from "./assets/Stars.svg";
+import verifyLogo from "./assets/verifyLogo.svg";
+import profileImg from "./assets/profileImg.svg";
 
+export const Annoucement = ({
+  profileName,
+  verifiedStatusProfile,
+  comentText,
+}) => {
+  return (
+    <div className="annoucement-container">
+      <div className="leftImgContent">
+        <img src={imgAnnoucement} alt="" />
+      </div>
 
-export const Annoucement = () => {
-    return (
-        <div className="annoucement-container">
+      <div className="leftComent">
+        <div className="titleComent">
+          <div className="profileImg">
+            <img src={profileImg} alt="" />
+          </div>
 
+          <div className="comentStats">
+            <div className="comentRating">
+              <img src={stars} alt="" />
+            </div>
 
-
+            <div className="profileName">
+              <span id="profileName">{profileName}</span>
+              <img src={verifyLogo} alt="" />
+              <span>{verifiedStatusProfile}</span>
+            </div>
+          </div>
         </div>
-    )
-}
+        <div className="comentText">
+          <span>{comentText}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
