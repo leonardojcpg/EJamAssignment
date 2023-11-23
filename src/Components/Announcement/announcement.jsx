1d046034f935cdebb759e4f93390378c9c00b60d
leonardoGomes <leonardojpinheirogomes@gmail.com>
2023-11-23 12:58:26 -0300

    refactor: Refactoring css on announcement component

--- src/Components/Announcement/announcement.jsx
+++ src/Components/Announcement/announcement.jsx
@@ -3,10 +3,14 @@ import { LeftSideAnnouncement } from "./LeftSideAnnouncement";
 import { RightSideAnnouncement } from "./RightSideAnnouncement";
 
 export const Announcement = () => {
-    return (
-      <div className="announcements-container">
+  return (
+    <div className="announcements-container">
+      <div className="left-container">
         <LeftSideAnnouncement />
+      </div>
+      <div className="right-container">
         <RightSideAnnouncement />
       </div>
-    );
-  };
+    </div>
+  );
+};
