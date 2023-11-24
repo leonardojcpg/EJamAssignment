6ae5e2c7cc548295f01268fa3df0870bbf23ce24
leonardoGomes <leonardojpinheirogomes@gmail.com>
2023-11-24 12:59:42 -0300

    feat: responsive header and secondary header

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,10 +1,56 @@
-import "./styles.css";
+import React from "react";
+import Slider from "react-slick";
+import { useMediaQuery } from "react-responsive";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import infoIconA from "./assets/infoIcon-a.svg";
 import infoIconB from "./assets/infoIcon-b.svg";
 import infoIconC from "./assets/infoIcon-c.svg";
 import infoIconD from "./assets/infoIcon-d.svg";
+import "./styles.css";
 
 export const Header = ({ infoTitleA, infoTitleB, infoTitleC, infoTitleD }) => {
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+
+  if (isMobile) {
+    const settings = {
+      dots: false,
+      infinite: true,
+      speed: 100,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+    };
+
+    return (
+      <Slider {...settings}>
+        <div className="header-container">
+          <div className="info-container">
+            <img src={infoIconA} alt="" />
+            <p>{infoTitleA}</p>
+          </div>
+        </div>
+        <div className="header-container">
+          <div className="info-container">
+            <img src={infoIconB} alt="" />
+            <p>{infoTitleB}</p>
+          </div>
+        </div>
+        <div className="header-container">
+          <div className="info-container">
+            <img src={infoIconC} alt="" />
+            <p>{infoTitleC}</p>
+          </div>
+        </div>
+        <div className="header-container">
+          <div className="info-container">
+            <img src={infoIconD} alt="" />
+            <p>{infoTitleD}</p>
+          </div>
+        </div>
+      </Slider>
+    );
+  }
+
   return (
     <div className="header-container">
       <div className="info-container">
